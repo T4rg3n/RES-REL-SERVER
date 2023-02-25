5fa38e9acca7d0788f613a2a7cdeb3dff4fada90
T4rg3n <34606799+T4rg3n@users.noreply.github.com>
2023-02-25 22:33:14 +0100

    FIX : Naming convention on API endpoints

--- routes/api.php
+++ routes/api.php
@@ -19,16 +19,16 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 // api/v1
-Route::group(['prefix' => 'V1', 'namespace' => 'App\Http\Controllers\V1'], function () {
+Route::group(['prefix' => 'v1', 'namespace' => 'App\Http\Controllers\V1'], function () {
     Route::apiResource('categories', CategorieController::class);
     Route::apiResource('commentaires', CommentaireController::class);
     Route::apiResource('favoris', FavorisController::class);
     Route::apiResource('groupes', GroupeController::class);
-    Route::apiResource('piece_jointes', PieceJointeController::class);
+    Route::apiResource('piecesJointes', PieceJointeController::class);
     Route::apiResource('relations', RelationController::class);
-    Route::apiResource('reponse_commentaires', ReponseCommentaireController::class);
+    Route::apiResource('reponsesCommentaires', ReponseCommentaireController::class);
     Route::apiResource('ressources', RessourceController::class);
     Route::apiResource('roles', RoleController::class);
-    Route::apiResource('type_relations', TypeRelationController::class);
+    Route::apiResource('typesRelation', TypeRelationController::class);
     Route::apiResource('utilisateurs', UtilisateurController::class);
 });
