048b4aa66c8880a90352c04c16e1502052bc7a98
T4rg3n <34606799+T4rg3n@users.noreply.github.com>
2023-02-26 14:39:55 +0100

    Removed autodeploy

--- routes/web.php
+++ routes/web.php
@@ -16,35 +16,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/hook', function () {
-    //load the deploy script
-    /**
-     * GIT DEPLOYMENT SCRIPT
-     *
-     * Used for automatically deploying websites via GitHub
-     *
-     */
-
-    // array of commands
-    $commands = array(
-        'echo $PWD',
-        'whoami',
-        'git pull',
-        'git status',
-        'git submodule sync',
-        'git submodule update',
-        'git submodule status',
-    );
-
-    // exec commands
-    $output = '';
-    foreach($commands AS $command){
-        $tmp = shell_exec($command);
-        
-        $output .= "<span style=\"color: #6BE234;\">\$</span><span style=\"color: #729FCF;\">{$command}\n</span><br />";
-        $output .= htmlentities(trim($tmp)) . "\n<br /><br />";
-    }
-    echo $output;
-
-});
