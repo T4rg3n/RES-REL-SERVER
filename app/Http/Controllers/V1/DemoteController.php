1322aa33ea37fce1c0ebbcd9c2dc4b997aa4591d
T4rg3n <34606799+T4rg3n@users.noreply.github.com>
2023-03-31 22:36:20 +0200

    WIP : Promote and demote controllers

--- app/Http/Controllers/V1/DemoteController.php
+++ app/Http/Controllers/V1/DemoteController.php
@@ -6,5 +6,16 @@ use Illuminate\Http\Request;
 
 class DemoteController extends Controller
 {
-    //
+    /**
+     * Edit the specified resource in storage.
+     * 
+     * @param  \Illuminate\Http\Request  $request
+     */
+    public function patch(Request $request)
+    {
+        $this->validate($request, [
+            'idUtilisateur' => 'required|integer',
+            'idRole' => 'required|integer',
+        ]);
+    }
 }
