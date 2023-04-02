bc8fc30b315237a507aee134fae84d1812ba2e73
T4rg3n <34606799+T4rg3n@users.noreply.github.com>
2023-04-02 18:58:15 +0200

    Deleted welcome view

--- routes/web.php
+++ routes/web.php
@@ -12,12 +12,7 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-/*
-Route::get('/', function () {
-    //redirect to /swagger/#/
-    return redirect('/swagger/#/');
-});
-*/
+
 Route::get('/', function () {
     return redirect('/swagger/#');
 });
