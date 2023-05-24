2453ace53535755fbacca1d0c6ec6c2b8febd0e3
T4rg3n <34606799+T4rg3n@users.noreply.github.com>
2023-05-24 14:19:48 +0200

    FIX : Updated mail assets

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,11 +2,7 @@
 <tr>
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
-@if (trim($slot) === 'Laravel')
 <img src="{{asset('/assets/mail-assets/icon.png')}}" class="logo" alt="Resrel Logo">
-@else
-{{ $slot }}
-@endif
 </a>
 </td>
 </tr>
