e311042f55c13e1f2c4e8d4c60d2b4fb82094664
T4rg3n <34606799+T4rg3n@users.noreply.github.com>
2023-06-06 16:05:09 +0200

    WIP : Authentication on /stats

--- app/Providers/ModelStatsServiceProvider.php
+++ app/Providers/ModelStatsServiceProvider.php
@@ -5,6 +5,8 @@ namespace App\Providers;
 use Illuminate\Support\Facades\Gate;
 use Jhumanj\LaravelModelStats\LaravelModelStats;
 use Jhumanj\LaravelModelStats\ModelStatsServiceProvider as Provider;
+use Laravel\Sanctum\PersonalAccessToken;
+use Illuminate\Support\Facades\Log;
 
 class ModelStatsServiceProvider extends Provider
 {
@@ -18,9 +20,24 @@ class ModelStatsServiceProvider extends Provider
     protected function gate(): void
     {
         Gate::define('viewModelStats', function ($user) {
-            return in_array($user->email, [
-                //
-            ]);
+
+            $request = request();
+            $token = $request->bearerToken();
+
+            //check if token is valid
+            if (empty($token)) {
+                return false;
+            }
+
+            //get user from token
+            $token = PersonalAccessToken::findToken($token);
+            return $token->tokenable->id == $user->id;
+            Log::debug("User: " . $user->id . " Token: " . $token->tokenable->id);
+
+
+            // return in_array($user->email, [
+            //     //
+            // ]);
         });
     }
 }
